623251433a8e8b3b4b3f8e65c4c9a13fddd4b610
James MacAdie (TT sandbox) <j.macadie@ttg.co.uk>
2024-02-06 22:40:38 +0000

    Project structure
    Moved things about a bit for better separation of concern. Should make
    adding the next functionality easier to manage

---
+++ src/error.rs
@@ -0,0 +1,263 @@
+use std::{fmt::Display, io, num::ParseIntError};
+
+pub type UnlockResult<T> = Result<T, UnlockError>;
+
+pub enum UnlockError {
+    FileOpen(io::Error),
+    Zip(zip::result::ZipError),
+    NoVBAFile,
+    CFBOpen(io::Error),
+    ProjectStructure(VBAProjectStructure),
+}
+
+impl From<io::Error> for UnlockError {
+    fn from(value: io::Error) -> Self {
+        Self::FileOpen(value)
+    }
+}
+
+impl From<zip::result::ZipError> for UnlockError {
+    fn from(value: zip::result::ZipError) -> Self {
+        Self::Zip(value)
+    }
+}
+
+impl Display for UnlockError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::FileOpen(e) => write!(f, "{e}"),
+            Self::Zip(e) => write!(
+                f,
+                "Problem with the zip representation of the supplied Excel file: {e}"
+            ),
+            Self::NoVBAFile => write!(
+                f,
+                "Could not find the 'xl/vbaProject.bin' file within the extracted archive"
+            ),
+            Self::CFBOpen(e) => write!(
+                f,
+                "There was a problem reading the CFB format vbaProject.bin file: {e}"
+            ),
+            Self::ProjectStructure(e) => write!(f, "{e}"),
+        }
+    }
+}
+
+pub enum VBAProjectStructure {
+    ProtectionState(VBAProtectionState),
+    Password(VBAPassword),
+    Visibility(VBAVisibility),
+}
+
+impl Display for VBAProjectStructure {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::ProtectionState(e) => write!(f, "{e}"),
+            Self::Password(e) => write!(f, "{e}"),
+            Self::Visibility(e) => write!(f, "{e}"),
+        }
+    }
+}
+
+impl From<VBAProtectionState> for VBAProjectStructure {
+    fn from(value: VBAProtectionState) -> Self {
+        Self::ProtectionState(value)
+    }
+}
+
+impl From<VBAPassword> for VBAProjectStructure {
+    fn from(value: VBAPassword) -> Self {
+        Self::Password(value)
+    }
+}
+
+impl From<VBAVisibility> for VBAProjectStructure {
+    fn from(value: VBAVisibility) -> Self {
+        Self::Visibility(value)
+    }
+}
+
+#[derive(Debug)]
+pub enum VBAProtectionState {
+    Decrypt(VBADecrypt),
+    Length(u32),
+    DataLength(usize),
+    ReservedBits([u8; 4]),
+}
+
+impl Display for VBAProtectionState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Decrypt(e) => write!(f, "{e}"),
+            Self::Length(l) => write!(f, "The length parameter for Data Encryption of the VBA Project Protection State MUST be 4, not {l}"),
+            Self::DataLength(l) => write!(f, "The Data array for Data Encryption of the VBA Project Protection State SHOULD be 4 bytes, not {l} bytes"),
+            Self::ReservedBits(data) => write!(f, "The upper 29 bits of data are reserved and MUST all be 0. Data decodeded to {:08b}{:08b}{:08b}{:08b}", data[0], data[1], data[2], data[3]),
+        }
+    }
+}
+
+impl From<VBADecrypt> for VBAProtectionState {
+    fn from(value: VBADecrypt) -> Self {
+        Self::Decrypt(value)
+    }
+}
+
+#[derive(Debug)]
+pub enum VBAPassword {
+    Decrypt(VBADecrypt),
+    None(VBAPasswordNone),
+    Hash(VBAPasswordHash),
+    PlainText(VBAPasswordPlain),
+}
+
+impl Display for VBAPassword {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Decrypt(e) => write!(f, "{e}"),
+            Self::None(e) => write!(f, "{e}"),
+            Self::Hash(e) => write!(f, "{e}"),
+            Self::PlainText(e) => write!(f, "{e}"),
+        }
+    }
+}
+
+impl From<VBADecrypt> for VBAPassword {
+    fn from(value: VBADecrypt) -> Self {
+        Self::Decrypt(value)
+    }
+}
+
+impl From<VBAPasswordNone> for VBAPassword {
+    fn from(value: VBAPasswordNone) -> Self {
+        Self::None(value)
+    }
+}
+
+impl From<VBAPasswordHash> for VBAPassword {
+    fn from(value: VBAPasswordHash) -> Self {
+        Self::Hash(value)
+    }
+}
+
+impl From<VBAPasswordPlain> for VBAPassword {
+    fn from(value: VBAPasswordPlain) -> Self {
+        Self::PlainText(value)
+    }
+}
+
+#[derive(Debug)]
+pub enum VBAPasswordNone {
+    DataLength(usize),
+    NotNull(u8),
+}
+
+impl Display for VBAPasswordNone {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::DataLength(l) => write!(
+                f,
+                "The data length for a VBA project without a password MUST be 1, not {l}"
+            ),
+            Self::NotNull(b) => write!(
+                f,
+                "The data value for a VBA project without a password MUST be 0x00, not 0x{b:02x}"
+            ),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum VBAPasswordHash {
+    DataLength(usize),
+    Reserved(u8),
+    Terminator(u8),
+    SaltNull([u8; 4], usize),
+    HashNull([u8; 20], usize),
+}
+
+impl Display for VBAPasswordHash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::DataLength(l) => write!(f, "The length of the VBA password hash data structure MUST be 29, not {l}"),
+            Self::Reserved(b) => write!(f, "The first byte of the VBA password hash data structure is reserved and MUST be 0xff, not 0x{b:02x}"),
+            Self::Terminator(b) => write!(f, "The final byte of the VBA password hash data structure is the terminator and MUST be 0x00, not 0x{b:02x}"),
+            Self::SaltNull(data, i) => write!(f, "The byte in position {i} of the salt {data:?} is being replaced with a null. It should have a value of 1 before update"),
+            Self::HashNull(data, i) => write!(f, "The byte in position {i} of the hash {data:?} is being replaced with a null. It should have a value of 1 before update"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum VBAPasswordPlain {
+    LengthToUsize(u32),
+    DataLength(usize, usize),
+    Terminator(u8),
+}
+
+impl Display for VBAPasswordPlain {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::LengthToUsize(_) => write!(f, "Could not convert the length to a usize (to index into the data array). This should never happen!"),
+            Self::DataLength(dl, l) => write!(f, "The length of the decrypted plain-text password {dl}, does not match the decrypted length {l}. Values cannot be trusted"),
+            Self::Terminator(b) => write!(f, "The plain-text password MUST be null terminated. We got 0x{b:02x}"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum VBAVisibility {
+    Decrypt(VBADecrypt),
+    Length(u32),
+    DataLength(usize),
+    InvalidState(u8),
+}
+
+impl Display for VBAVisibility {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Decrypt(e) => write!(f, "{e}"),
+            Self::Length(l) => write!(f, "VBA Visibility should decrypt to a length of 1, not {l}"),
+            Self::DataLength(l) => write!(
+                f,
+                "VBA Visibility should decrypt to a data length of 1, not {l}"
+            ),
+            Self::InvalidState(s) => write!(
+                f,
+                "VBA Visibility only has two valid values: 0x00 and 0xff. 0x{s:02x} was found"
+            ),
+        }
+    }
+}
+
+impl From<VBADecrypt> for VBAVisibility {
+    fn from(value: VBADecrypt) -> Self {
+        Self::Decrypt(value)
+    }
+}
+
+#[derive(Debug)]
+pub enum VBADecrypt {
+    InvalidHex(String),
+    Version(u8),
+}
+
+impl From<ParseIntError> for VBADecrypt {
+    fn from(value: ParseIntError) -> Self {
+        Self::InvalidHex(format!("{value}"))
+    }
+}
+
+impl Display for VBADecrypt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidHex(s) => write!(
+                f,
+                "Cannot apply VBA data decryption as supplied value is not valid hex: {s}"
+            ),
+            Self::Version(v) => {
+                writeln!(f, "VBA Data Encryption Version MUST be 2 according to the spec: https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/7e9d84fe-86e3-46d6-aaff-8388e72c0168")?;
+                writeln!(f, "Got {v}")
+            }
+        }
+    }
+}
---
+++ src/read.rs
@@ -0,0 +1,326 @@
+use std::io::BufRead;
+
+use crate::error;
+use crate::error::VBAProjectStructure;
+use crate::InMemStream;
+use vba_password::ProjectPassword;
+use vba_protection_state::ProjectProtectionState;
+use vba_visibility::ProjectVisibililyState;
+
+pub fn print_info(project: InMemStream) -> Result<(), VBAProjectStructure> {
+    for line in project.lines().flatten() {
+        if line.starts_with("CMG=") {
+            let protection_state: ProjectProtectionState = line.parse()?;
+            println!("{protection_state}");
+        }
+        if line.starts_with("DPB=") {
+            let password: ProjectPassword = line.parse()?;
+            println!("{password}");
+        }
+        if line.starts_with("GC=") {
+            let visibility_state: ProjectVisibililyState = line.parse()?;
+            println!("{visibility_state}");
+        }
+    }
+    Ok(())
+}
+
+mod vba_protection_state {
+    // https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/55e770e2-e1a4-4d1c-a8a4-dcfca27d6663
+    use super::decrypt;
+    use crate::error::VBAProtectionState;
+    use std::{fmt::Display, str::FromStr};
+
+    pub struct ProjectProtectionState {
+        user: bool,
+        host: bool,
+        vbe: bool,
+    }
+
+    impl FromStr for ProjectProtectionState {
+        type Err = VBAProtectionState;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let hex_str = s.trim_start_matches("CMG=\"").trim_end_matches('"');
+            let (data, length) = decrypt(hex_str)?;
+            if length != 4 {
+                return Err(VBAProtectionState::Length(length));
+            }
+            if data.len() != 4 {
+                return Err(VBAProtectionState::DataLength(data.len()));
+            }
+            if data[0] > 7 || data[1] != 0 || data[2] != 0 || data[3] != 0 {
+                return Err(VBAProtectionState::ReservedBits([
+                    data[0], data[1], data[2], data[3],
+                ]));
+            }
+            let user_protected = data[0] & 1 == 1;
+            let host_protected = data[0] & 2 == 2;
+            let vbe_protected = data[0] & 4 == 4;
+            Ok(Self {
+                user: user_protected,
+                host: host_protected,
+                vbe: vbe_protected,
+            })
+        }
+    }
+
+    impl Display for ProjectProtectionState {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            writeln!(f, "Project Protection State:")?;
+            writeln!(f, "  User Protected: {}", self.user)?;
+            writeln!(f, "  Host Protected: {}", self.host)?;
+            writeln!(f, "  VBE Protected: {}", self.vbe)?;
+            Ok(())
+        }
+    }
+}
+
+pub mod vba_password {
+    // https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/79685426-30fe-43cd-9cbf-7f161c3de7d8
+    use super::decrypt;
+    use crate::error;
+    use crate::WORDS;
+    use sha1::{Digest, Sha1};
+    use std::{fmt::Display, str::FromStr};
+
+    pub enum ProjectPassword {
+        None,
+        Hash([u8; 4], [u8; 20]),
+        PlainText(String),
+    }
+
+    impl FromStr for ProjectPassword {
+        type Err = error::VBAPassword;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let hex_str = s.trim_start_matches("DPB=\"").trim_end_matches('"');
+            let (data, length) = decrypt(hex_str)?;
+            Ok(match length {
+                1 => Self::new_none(&data)?,
+                29 => Self::new_hash(&data)?,
+                l => Self::new_plain(&data, l)?,
+            })
+        }
+    }
+
+    impl ProjectPassword {
+        fn new_none(data: &[u8]) -> Result<Self, error::VBAPasswordNone> {
+            if data.len() != 1 {
+                return Err(error::VBAPasswordNone::DataLength(data.len()));
+            }
+            if data.first() != Some(0x00).as_ref() {
+                return Err(error::VBAPasswordNone::NotNull(data[0]));
+            }
+            Ok(Self::None)
+        }
+
+        fn new_hash(data: &[u8]) -> Result<Self, error::VBAPasswordHash> {
+            // https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/9d9f81e6-f92e-4338-a242-d38c1fcceed6
+            if data.len() != 29 {
+                return Err(error::VBAPasswordHash::DataLength(data.len()));
+            }
+            if data.first() != Some(0xff).as_ref() {
+                return Err(error::VBAPasswordHash::Reserved(data[0]));
+            }
+            if data.last() != Some(0x00).as_ref() {
+                return Err(error::VBAPasswordHash::Terminator(data[28]));
+            }
+
+            let mut salt = [0; 4];
+            salt.clone_from_slice(&data[4..8]);
+
+            let mut hash = [0; 20];
+            hash.clone_from_slice(&data[8..28]);
+
+            // https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/5797c2e1-4c86-4f44-89b4-1edb30da00cc
+            // Add nulls to salt
+            let mut grbitkey = data[1] & 0x0f;
+            for (i, byte) in salt.iter_mut().enumerate() {
+                if grbitkey & 1 == 0 {
+                    if *byte != 0x01 {
+                        return Err(error::VBAPasswordHash::SaltNull(salt, i));
+                    }
+                    *byte = 0;
+                }
+                grbitkey >>= 1;
+            }
+
+            // Add nulls to hash
+            let mut grbithashnull = u32::from(data[1]) >> 4;
+            grbithashnull |= u32::from(data[2]) << 4;
+            grbithashnull |= u32::from(data[3]) << 12;
+            for (i, byte) in hash.iter_mut().enumerate() {
+                if grbithashnull & 1 == 0 {
+                    if *byte != 0x01 {
+                        return Err(error::VBAPasswordHash::HashNull(hash, i));
+                    }
+                    *byte = 0;
+                }
+                grbithashnull >>= 1;
+            }
+
+            Ok(Self::Hash(salt, hash))
+        }
+
+        #[allow(dead_code)]
+        fn crack_password(&self) -> Option<String> {
+            if let Self::Hash(salt, hash) = self {
+                let mut hasher = Sha1::new();
+                for trial in WORDS {
+                    let mut salted: Vec<u8> = trial.as_bytes().to_owned();
+                    salted.extend_from_slice(salt);
+                    hasher.update(salted);
+                    if hasher.finalize_reset()[..] == *hash {
+                        return Some(trial.to_owned());
+                    }
+                }
+            }
+            None
+        }
+
+        fn new_plain(data: &[u8], length: u32) -> Result<Self, error::VBAPasswordPlain> {
+            let Ok(length) = usize::try_from(length) else {
+                return Err(error::VBAPasswordPlain::LengthToUsize(length));
+            };
+            if data.len() != length {
+                return Err(error::VBAPasswordPlain::DataLength(data.len(), length));
+            }
+            if data.last() != Some(0x00).as_ref() {
+                return Err(error::VBAPasswordPlain::Terminator(data[length - 1]));
+            }
+            let password = String::from_utf8_lossy(&data[0..(length - 2)]).to_string();
+            Ok(Self::PlainText(password))
+        }
+    }
+
+    impl Display for ProjectPassword {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "Project Password: ")?;
+            match self {
+                Self::None => write!(f, "None")?,
+                Self::Hash(salt, hash) => {
+                    writeln!(f, "Hashed (SHA1)")?;
+                    write!(f, "  Salt: ")?;
+                    for byte in salt {
+                        write!(f, "{byte:02x}")?;
+                    }
+                    writeln!(f)?;
+                    write!(f, "  SHA1 Hash: ")?;
+                    for byte in hash {
+                        write!(f, "{byte:02x}")?;
+                    }
+                    writeln!(f)?;
+                }
+                Self::PlainText(password) => write!(f, "{password} (plain-text)")?,
+            }
+            Ok(())
+        }
+    }
+}
+
+pub mod vba_visibility {
+    // https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/690c96e8-e862-497f-bb7d-5eacf4dc742a
+    use super::decrypt;
+    use crate::error;
+    use std::{fmt::Display, str::FromStr};
+
+    pub enum ProjectVisibililyState {
+        NotVisible,
+        Visible,
+    }
+
+    impl FromStr for ProjectVisibililyState {
+        type Err = error::VBAVisibility;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let hex_str = s.trim_start_matches("GC=\"").trim_end_matches('"');
+            let (data, length) = decrypt(hex_str)?;
+            if length != 1 {
+                return Err(error::VBAVisibility::Length(length));
+            }
+            if data.len() != 1 {
+                return Err(error::VBAVisibility::DataLength(data.len()));
+            }
+            match data.first() {
+                Some(0x00) => Ok(Self::NotVisible),
+                Some(0xff) => Ok(Self::Visible),
+                Some(x) => Err(error::VBAVisibility::InvalidState(*x)),
+                None => unreachable!(),
+            }
+        }
+    }
+
+    impl Display for ProjectVisibililyState {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            writeln!(f, "Project Visibility:")?;
+            match self {
+                Self::Visible => writeln!(f, "  Visible")?,
+                Self::NotVisible => writeln!(f, "  Not Visible")?,
+            }
+            Ok(())
+        }
+    }
+}
+
+fn decrypt(hex: &str) -> Result<(Vec<u8>, u32), error::VBADecrypt> {
+    // https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/a2ad3aa7-e180-4ccb-8511-7e0eb49a0ad9
+    // https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/7e9d84fe-86e3-46d6-aaff-8388e72c0168
+
+    if hex.bytes().any(|x| !x.is_ascii_hexdigit()) {
+        return Err(error::VBADecrypt::InvalidHex(hex.to_owned()));
+    }
+
+    let seed = u8::from_str_radix(&hex[0..2], 16)?;
+    let version_enc = u8::from_str_radix(&hex[2..4], 16)?;
+    let project_key_enc = u8::from_str_radix(&hex[4..6], 16)?;
+
+    let version = seed ^ version_enc;
+    if version != 2 {
+        return Err(error::VBADecrypt::Version(version));
+    }
+    let project_key = seed ^ project_key_enc;
+    let ignored_length = ((seed & 6) >> 1).into();
+
+    let mut unencrypted_byte_1 = project_key;
+    let mut encrypted_byte_1 = project_key_enc;
+    let mut encrypted_byte_2 = version_enc;
+
+    // Convert remaining string into a u8 (byte) iterator
+    let data_bytes = hex[6..].as_bytes().chunks_exact(2).map(|x| {
+        let upper = match x[0] {
+            val if val.is_ascii_digit() => val - b'0',
+            val if val.is_ascii_lowercase() => val - b'a' + 10,
+            val if val.is_ascii_uppercase() => val - b'A' + 10,
+            _ => unreachable!(),
+        };
+        let lower = match x[1] {
+            val if val.is_ascii_digit() => val - b'0',
+            val if val.is_ascii_lowercase() => val - b'a' + 10,
+            val if val.is_ascii_uppercase() => val - b'A' + 10,
+            _ => unreachable!(),
+        };
+        (upper << 4) | lower
+    });
+
+    // Generate the length & data
+    let mut data = Vec::new();
+    let mut length = 0;
+    for (i, byte_enc) in data_bytes.enumerate() {
+        let byte = byte_enc ^ (encrypted_byte_2 + unencrypted_byte_1);
+        encrypted_byte_2 = encrypted_byte_1;
+        encrypted_byte_1 = byte_enc;
+        unencrypted_byte_1 = byte;
+        match i {
+            x if x < ignored_length => (), // Ignore these bytes
+            x if x < ignored_length + 4 => {
+                let b = u32::from(byte);
+                let shift = 4 * (x - ignored_length);
+                length |= b << shift;
+            }
+            _ => data.push(byte),
+        }
+    }
+
+    Ok((data, length))
+}
