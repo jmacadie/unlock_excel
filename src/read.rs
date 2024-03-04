2d207503048a386bf0b1d93622bda695240f1cc2
James MacAdie (TT sandbox) <j.macadie@ttg.co.uk>
2024-03-04 15:01:03 +0000

    Add documentation for the public facing parts of the library

--- src/read.rs
+++ src/read.rs
@@ -8,12 +8,61 @@ use crate::ovba::records::project::{Password, Project};
 use sha1::{Digest, Sha1};
 use zip::ZipArchive;
 
+/// Print the VBA project locked status to standard out.
+/// This is the version for Excel files since 2003 i.e. xlsm and xlsb
+///
+/// The decode flag, if set to true, will trigger an attempt to decode a SHA hashed password. This
+/// is done by testing against [a list of 1.7 million common passwords](https://github.com/openwall/john/blob/bleeding-jumbo/run/password.lst)
+///
+/// # Errors
+/// Will return an error in the following situations:
+/// - The file cannot be opened
+/// - The file is cannot be opened as a zip file: Excel files since 2003 are really zip files. The
+/// contents within the zip file changes depending on the Excel file format used: xlsx, xlsm, xlsb
+/// - If there is no VBA file within the zip archive, found at "/xl/vbaProject.bin". Note that an
+/// xlsm file saved with no macros will be missing this file, as will any xlsx file. In the former
+/// case, the code really ought to handle the "error" more gracefully
+/// - If the VBA file within the archive cannot be opened as a [Compound File Binary (CFB)](https://learn.microsoft.com/en-us/openspecs/windows_protocols/MS-CFB/53989ce4-7b05-4f8d-829b-d08d6148375b).
+/// This file format stores the data of a file as a mini file system. The data of each "file"
+/// within the overall file is stored as streams. These streams are written to 512 byte sectors, or
+/// 64 byte chunks of the mini-stream. In either case, the sectors or the mini-stream, the stream
+/// is not guaranteed to be written to contiguous memory, so it is important that the file is
+/// properly opened as a CFB file in order to read the streams correctly
+/// - If the [PROJECT stream](https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/cc848a02-6f87-49a4-ad93-6edb3103f593),
+/// which holds the VBA locked status, cannot be found within the overall VBA CFB file
+/// - If the [PROJECT stream cannot be parsed](https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/cc848a02-6f87-49a4-ad93-6edb3103f593)
+/// into its constituent parts correctly
 pub fn print_xl(filename: &Path, decode: bool) -> UnlockResult<()> {
     let (project, decoded_password) = xl_project(filename, decode)?;
     print_info(&project, decode, decoded_password);
     Ok(())
 }
 
+/// Parse an Excel file into an [`ovba::records::project::Project`].
+/// This is exposed to allow for integration testing.
+/// This is the version for Excel files since 2003 i.e. xlsm and xlsb
+///
+/// The decode flag, if set to true, will trigger an attempt to decode a SHA hashed password. This
+/// is done by testing against [a list of 1.7 million common passwords](https://github.com/openwall/john/blob/bleeding-jumbo/run/password.lst)
+///
+/// # Errors
+/// Will return an error in the following situations:
+/// - The file cannot be opened
+/// - The file is cannot be opened as a zip file: Excel files since 2003 are really zip files. The
+/// contents within the zip file changes depending on the Excel file format used: xlsx, xlsm, xlsb
+/// - If there is no VBA file within the zip archive, found at "/xl/vbaProject.bin". Note that an
+/// xlsm file saved with no macros will be missing this file, as will any xlsx file. In the former
+/// case, the code really ought to handle the "error" more gracefully
+/// - If the VBA file within the archive cannot be opened as a [Compound File Binary (CFB)](https://learn.microsoft.com/en-us/openspecs/windows_protocols/MS-CFB/53989ce4-7b05-4f8d-829b-d08d6148375b).
+/// This file format stores the data of a file as a mini file system. The data of each "file"
+/// within the overall file is stored as streams. These streams are written to 512 byte sectors, or
+/// 64 byte chunks of the mini-stream. In either case, the sectors or the mini-stream, the stream
+/// is not guaranteed to be written to contiguous memory, so it is important that the file is
+/// properly opened as a CFB file in order to read the streams correctly
+/// - If the [PROJECT stream](https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/cc848a02-6f87-49a4-ad93-6edb3103f593),
+/// which holds the VBA locked status, cannot be found within the overall VBA CFB file
+/// - If the [PROJECT stream cannot be parsed](https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/cc848a02-6f87-49a4-ad93-6edb3103f593)
+/// into its constituent parts correctly
 pub fn xl_project(filename: &Path, decode: bool) -> UnlockResult<(Project, Option<String>)> {
     let zipfile = File::open(filename)?;
     let mut archive = zip::ZipArchive::new(zipfile)?;
@@ -27,12 +76,51 @@ pub fn xl_project(filename: &Path, decode: bool) -> UnlockResult<(Project, Optio
     Ok((project, decoded_password))
 }
 
+/// Print the VBA project locked status to standard out.
+/// This is the version for Excel files between 1997 & 2003 i.e. xls
+///
+/// The decode flag, if set to true, will trigger an attempt to decode a SHA hashed password. This
+/// is done by testing against [a list of 1.7 million common passwords](https://github.com/openwall/john/blob/bleeding-jumbo/run/password.lst)
+///
+/// # Errors
+/// Will return an error in the following situations:
+/// - The file cannot be opened
+/// - If the file cannot be opened as a [Compound File Binary (CFB)](https://learn.microsoft.com/en-us/openspecs/windows_protocols/MS-CFB/53989ce4-7b05-4f8d-829b-d08d6148375b).
+/// This file format stores the data of a file as a mini file system. The data of each "file"
+/// within the overall file is stored as streams. These streams are written to 512 byte sectors, or
+/// 64 byte chunks of the mini-stream. In either case, the sectors or the mini-stream, the stream
+/// is not guaranteed to be written to contiguous memory, so it is important that the file is
+/// properly opened as a CFB file in order to read the streams correctly
+/// - If the [PROJECT stream](https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/cc848a02-6f87-49a4-ad93-6edb3103f593),
+/// which holds the VBA locked status, cannot be found within the overall CFB file
+/// - If the [PROJECT stream cannot be parsed](https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/cc848a02-6f87-49a4-ad93-6edb3103f593)
+/// into its constituent parts correctly
 pub fn print_xl_97(filename: &Path, decode: bool) -> UnlockResult<()> {
     let (project, decoded_password) = xl_97_project(filename, decode)?;
     print_info(&project, decode, decoded_password);
     Ok(())
 }
 
+/// Parse an Excel file into an [`ovba::records::project::Project`].
+/// This is exposed to allow for integration testing.
+/// This is the version for Excel files between 1997 & 2003 i.e. xls
+///
+/// The decode flag, if set to true, will trigger an attempt to decode a SHA hashed password. This
+/// is done by testing against [a list of 1.7 million common passwords](https://github.com/openwall/john/blob/bleeding-jumbo/run/password.lst)
+///
+/// # Errors
+/// Will return an error in the following situations:
+/// - The file cannot be opened
+/// - If the file cannot be opened as a [Compound File Binary (CFB)](https://learn.microsoft.com/en-us/openspecs/windows_protocols/MS-CFB/53989ce4-7b05-4f8d-829b-d08d6148375b).
+/// This file format stores the data of a file as a mini file system. The data of each "file"
+/// within the overall file is stored as streams. These streams are written to 512 byte sectors, or
+/// 64 byte chunks of the mini-stream. In either case, the sectors or the mini-stream, the stream
+/// is not guaranteed to be written to contiguous memory, so it is important that the file is
+/// properly opened as a CFB file in order to read the streams correctly
+/// - If the [PROJECT stream](https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/cc848a02-6f87-49a4-ad93-6edb3103f593),
+/// which holds the VBA locked status, cannot be found within the overall CFB file
+/// - If the [PROJECT stream cannot be parsed](https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/cc848a02-6f87-49a4-ad93-6edb3103f593)
+/// into its constituent parts correctly
 pub fn xl_97_project(filename: &Path, decode: bool) -> UnlockResult<(Project, Option<String>)> {
     let mut file = cfb::open(filename).map_err(UnlockError::CFBOpen)?;
     let project_stream = file.open_stream(consts::CFB_VBA_PATH)?;
@@ -43,21 +131,23 @@ pub fn xl_97_project(filename: &Path, decode: bool) -> UnlockResult<(Project, Op
     Ok((project, decoded_password))
 }
 
-pub fn zip_to_raw_vba<R: std::io::Read + std::io::Seek>(
+/// Read the uncompressed bytes of the vbaProject.bin file into an in-memory cursor
+///
+/// Need this as `ZipFile` does not implement Seek, so we cannot call `open_stream`
+/// on a `CompoundFile` that is built directly off the `ZipFile`
+pub(crate) fn zip_to_raw_vba<R: std::io::Read + std::io::Seek>(
     zip: &mut ZipArchive<R>,
 ) -> UnlockResult<Cursor<Vec<u8>>> {
     let Ok(mut vba_file) = zip.by_name(consts::ZIP_VBA_PATH) else {
         return Err(UnlockError::NoVBAFile);
     };
 
-    // Read the uncompressed bytes of the vbaProject.bin file into an in-memory cursor
-    // Need this as ZipFile does not implement Seek, so we cannot call open_stream
-    // on a CompoundFile that is built directly off the ZipFile
     let mut buffer = Vec::with_capacity(1024);
     let _ = vba_file.read_to_end(&mut buffer);
     Ok(Cursor::new(buffer))
 }
 
+/// Internal function to print the results of the Project stuct to stdout consistently
 fn print_info(p: &Project, decode: bool, decoded: Option<String>) {
     if p.is_locked() {
         match p.password() {
--- src/remove.rs
+++ src/remove.rs
@@ -8,20 +8,37 @@ use std::io::{BufRead, Write};
 use std::path::Path;
 use std::path::PathBuf;
 
-pub fn xl_97(filename: &Path, inplace: bool) -> UnlockResult<()> {
-    let mut file = if inplace {
-        cfb::open_rw(filename).map_err(UnlockError::CFBOpen)?
-    } else {
-        let new_file = replacement_filename(filename)?;
-        std::fs::copy(filename, &new_file)?;
-        cfb::open_rw(new_file).map_err(UnlockError::CFBOpen)?
-    };
-    let project = file.open_stream(consts::CFB_VBA_PATH)?;
-    let replacement = unlocked_project(project)?;
-    let mut project = file.create_stream(consts::CFB_VBA_PATH)?;
-    Ok(project.write_all(&replacement)?)
-}
-
+/// Remove the VBA protection from an Excel file
+/// This is the version for Excel files since 2003 i.e. xlsm and xlsb
+///
+/// The inplace flag, if set to true, will overwrite the source file with a modified unlocked
+/// version. It is recommended to take a back-up of the file before doing this as the tool is
+/// relatively new and untested. It may corrupt your file.
+///
+/// Alternatively, pass false for the inplace flag to get a copy of the source file. It will have
+/// the same name as the source file, but have '_unlocked' appended to the filename.
+///
+/// # Errors
+/// Will return an error in the following situations:
+/// - The file cannot be opened
+/// - The file is cannot be opened as a zip file: Excel files since 2003 are really zip files. The
+/// contents within the zip file changes depending on the Excel file format used: xlsx, xlsm, xlsb
+/// - There is no VBA file within the zip archive, found at "/xl/vbaProject.bin". Note that an
+/// xlsm file saved with no macros will be missing this file, as will any xlsx file. In the former
+/// case, the code really ought to handle the "error" more gracefully
+/// - The VBA file within the archive cannot be opened as a [Compound File Binary (CFB)](https://learn.microsoft.com/en-us/openspecs/windows_protocols/MS-CFB/53989ce4-7b05-4f8d-829b-d08d6148375b).
+/// This file format stores the data of a file as a mini file system. The data of each "file"
+/// within the overall file is stored as streams. These streams are written to 512 byte sectors, or
+/// 64 byte chunks of the mini-stream. In either case, the sectors or the mini-stream, the stream
+/// is not guaranteed to be written to contiguous memory, so it is important that the file is
+/// properly opened as a CFB file in order to read the streams correctly
+/// - The [PROJECT stream](https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/cc848a02-6f87-49a4-ad93-6edb3103f593),
+/// which holds the VBA locked status, cannot be found within the overall VBA CFB file
+/// - The updated project stream cannot be written back to the CFB file
+/// - An updated zip file cannot be created
+/// - The updated VBA CFB file cannot be written to the new zip file
+/// - The rest of the source zip file cannot be copied across as raw to the new zip file
+/// - If being run inplace, the new zip file cannot be copied back over the original
 pub fn xl(filename: &Path, inplace: bool) -> UnlockResult<()> {
     let zipfile = File::open(filename)?;
     let mut archive = zip::ZipArchive::new(zipfile)?;
@@ -70,6 +87,42 @@ pub fn xl(filename: &Path, inplace: bool) -> UnlockResult<()> {
     }
 
     Ok(())
+}
+
+/// Remove the VBA protection from an Excel file
+/// This is the version for Excel files between 1997 & 2003 i.e. xls
+///
+/// The inplace flag, if set to true, will overwrite the source file with a modified unlocked
+/// version. It is recommended to take a back-up of the file before doing this as the tool is
+/// relatively new and untested. It may corrupt your file.
+///
+/// Alternatively, pass false for the inplace flag to get a copy of the source file. It will have
+/// the same name as the source file, but have '_unlocked' appended to the filename.
+///
+/// # Errors
+/// Will return an error in the following situations:
+/// - The file cannot be copied (for not inplace only) or opened for read/write
+/// - The file cannot be opened as a [Compound File Binary (CFB)](https://learn.microsoft.com/en-us/openspecs/windows_protocols/MS-CFB/53989ce4-7b05-4f8d-829b-d08d6148375b).
+/// This file format stores the data of a file as a mini file system. The data of each "file"
+/// within the overall file is stored as streams. These streams are written to 512 byte sectors, or
+/// 64 byte chunks of the mini-stream. In either case, the sectors or the mini-stream, the stream
+/// is not guaranteed to be written to contiguous memory, so it is important that the file is
+/// properly opened as a CFB file in order to read the streams correctly
+/// - The [PROJECT stream](https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-ovba/cc848a02-6f87-49a4-ad93-6edb3103f593),
+/// which holds the VBA locked status, cannot be found within the overall VBA CFB file
+/// - The updated project stream cannot be written back to the CFB file
+pub fn xl_97(filename: &Path, inplace: bool) -> UnlockResult<()> {
+    let mut file = if inplace {
+        cfb::open_rw(filename).map_err(UnlockError::CFBOpen)?
+    } else {
+        let new_file = replacement_filename(filename)?;
+        std::fs::copy(filename, &new_file)?;
+        cfb::open_rw(new_file).map_err(UnlockError::CFBOpen)?
+    };
+    let project = file.open_stream(consts::CFB_VBA_PATH)?;
+    let replacement = unlocked_project(project)?;
+    let mut project = file.create_stream(consts::CFB_VBA_PATH)?;
+    Ok(project.write_all(&replacement)?)
 }
 
 fn unlocked_project<T: std::io::Read + std::io::Seek>(
